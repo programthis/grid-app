668e0776db5f51f57b3175ff7bd7a99ef9186004
programthis <kylebachan@gmail.com>
2023-05-23 12:28:32 -0400

    Using hooks to set current user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { GridStack } from 'gridstack';
 import 'gridstack/dist/gridstack.min.css';
 import './App.scss';
-import Grid from "./Grid";
+import Widget from "./Widget";
 
 function App() {
+    const [current_user, setCurrentUser] = useState(false);
+
     useEffect(() => {
         let windowWidth = window.innerWidth,
             numOfColumns = Math.floor(windowWidth / 100);
@@ -31,9 +33,30 @@ function App() {
         };
     });
 
+    const loggingInUser = () => {
+        console.log("setting current user...");
+        setCurrentUser(true);
+    }
+
     return (
         <div className="App">
-            <Grid />
+            <div className="topContainer">
+                {
+                    current_user ?
+                    <div>
+                        <button onClick={() => this.loadGrid()}>Load</button>
+                        <button onClick={() => this.saveGrid()}>Save</button>
+                    </div>
+                    :
+                    <button onClick={loggingInUser}>Login</button>    
+                }
+            </div>
+            <div className="grid-stack">
+                <Widget options={{width: "1", height: "1", name: "Luke", background: "#67d967", x_pos: "0", y_pos: "0"}} />
+                <Widget options={{width: "1", height: "1", name: "Leia", background: "#3371cd", x_pos: "1", y_pos: "0"}} />
+                <Widget options={{width: "1", height: "1", name: "Marv", background: "#ebeb5b", x_pos: "2", y_pos: "0"}} />
+                <Widget options={{width: "2", height: "3", name: "Darth", background: "#db2c2c", x_pos: "1", y_pos: "1"}} />
+            </div>
         </div>
     );
 }
