de2c3bcecfd2890a444a44af046cc66e60f28cd1
programthis <kylebachan@gmail.com>
2023-05-25 15:55:53 -0400

    Working on column change

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
             setBackgroundSize(backgroundCalculation);
             var grid = GridStack.init({
                 float: true,
-                gridBackground: true,
                 column: numOfColumns
             });
             gridRef.current = grid;
@@ -35,11 +34,11 @@ function App() {
             let grid = gridRef.current;
             if (grid) {
                 // ********seems to be an issue with below two lines, opening ticket in gridstack.js ****
-                // grid.destroy(false);
-                // initializeGrid();
+                grid.destroy(false);
+                initializeGrid();
 
                 // using jank option as above seems to be broken in library package
-                window.location.reload(false);
+                // window.location.reload(false);
             }
         }
         initializeGrid();
